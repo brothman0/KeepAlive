4a3f74736e24589a98ed4cea647888200182a813
Benjamin Rothman <whiskman@gmail.com>
2022-07-19 13:24:29 -0400

    removed old files

--- KeepAlive/KeepAlive.Client/External/Resources/GetMonitorFromPoint/MonitorFromPointFlag.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace KeepAlive.External.Resources.GetMonitorFromPoint;
-
-public enum MonitorFromPointFlag : uint
-{
-    DefaultToNull = 0x00000000,
-    DefaultToPrimary = 0x00000001,
-    DefaultToNearest = 0x00000002
-}
